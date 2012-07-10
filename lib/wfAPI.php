2a8ab10d592f7cd8a0bda71aab0464d6646941b4
mmaunder <mmaunder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-10 09:48:51 +0000

    Fixed bug that caused "Could not get the administrator’s user ID. Scan can’t continue."
    git-svn-id: https://plugins.svn.wordpress.org/wordfence/tags/3.0.8@569915 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/wfAPI.php
+++ lib/wfAPI.php
@@ -2,40 +2,33 @@
 require_once('wordfenceConstants.php');
 require_once('wordfenceClass.php');
 class wfAPI {
-	public $errorMsg = false;
-	public $lastURLError = '';
 	public $lastHTTPStatus = '';
 	public $lastCurlErrorNo = '';
 	private $curlDataWritten = 0;
 	private $curlContent = 0;
 	private $APIKey = '';
 	private $wordpressVersion = '';
+	private static $maintMsg = "The Wordfence scanning server may be down for maintenance.";
 	public function __construct($apiKey, $wordpressVersion){
 		$this->APIKey = $apiKey;
 		$this->wordpressVersion = $wordpressVersion;
 	}
 	public function call($action, $getParams = array(), $postParams = array()){
-		$this->errorMsg = false;
-		$json = $this->getURL(WORDFENCE_API_URL . '/v' . WORDFENCE_VERSION . '/?' . $this->makeAPIQueryString() . '&' . http_build_query(
+		$json = $this->getURL($this->getAPIURL() . '/v' . WORDFENCE_API_VERSION . '/?' . $this->makeAPIQueryString() . '&' . http_build_query(
 			array_merge(
 				array('action' => $action),
 				$getParams	
 				)), $postParams);
 		if(! $json){
-			if($this->lastHTTPStatus == '502' || $this->lastCurlErrorNo == 7){
-				$this->errorMsg = "Wordfence is currently down for maintenance. Please try again later.";
-			} else {
-				$this->errorMsg = "We could not fetch data from the API when calling '$action': " . $this->lastURLError;
-			}
-			return false;
+			throw new Exception(self::$maintMsg);
 		}
 
 		$dat = json_decode($json, true);
 		if(! is_array($dat)){
-			$this->errorMsg = "We could not understand the Wordfence API response when calling '$action'.";
+			throw new Exception("We could not understand the Wordfence API response when calling '$action'.");
 		}
-		if(empty($dat['errorMsg']) === false){
-			$this->errorMsg = $dat['errorMsg'];
+		if(is_array($dat) && isset($dat['errorMsg'])){
+			throw new Exception($dat['errorMsg']);
 		}
 		return $dat;
 	}
@@ -48,13 +41,12 @@ class wfAPI {
 		}
 	}
 	protected function getURL($url, $postParams = array()){
-		$this->lastURLError = '';
 		if(function_exists('curl_init')){
 			$this->curlDataWritten = 0;
 			$this->curlContent = "";
 			$curl = curl_init($url);
 			curl_setopt ($curl, CURLOPT_TIMEOUT, 300);
-			curl_setopt ($curl, CURLOPT_USERAGENT, "Wordfence.com UA " . WORDFENCE_VERSION);
+			curl_setopt ($curl, CURLOPT_USERAGENT, "Wordfence.com UA " . (defined('WORDFENCE_VERSION') ? WORDFENCE_VERSION : '[Unknown version]') );
 			curl_setopt ($curl, CURLOPT_RETURNTRANSFER, TRUE);
 			curl_setopt ($curl, CURLOPT_HEADER, 0);
 			curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -70,45 +62,76 @@ class wfAPI {
 				curl_close($curl);
 				return $this->curlContent;
 			} else {
-				$this->lastURLError = "HTTP status $httpStatus from server. " . curl_error($curl);
-				$this->lastHTTPStatus = $httpStatus;
+				$cerror = curl_error($curl);
 				curl_close($curl);
-				return false;
+				throw new Exception(self::$maintMsg . " Got HTTP status code [$httpStatus] and curl error: $cerror");
 			}
 		} else {
-			$data = @file_get_contents ($url);
+			$data = $this->fileGet($url, $postParams);
 			if($data === false){
 				$err = error_get_last();
-				$this->lastURLError = $err;
-				return false;
+				throw new Exception(self::$maintMsg . " Got HTTP error: " . $err);
 			}
 			return $data;
 		}
 
 	}
+	private function fileGet($url, $postParams){
+		$body = "";
+		if(is_array($postParams)){
+			$bodyArr = array();
+			foreach($postParams as $key => $val){
+				$bodyArr[] = urlencode($key) . '=' . urlencode($val);
+			}
+			$body = implode('&', $bodyArr);
+		} else {
+			$body = $postParams;
+		}
+		$opts = array('http' =>
+				array(
+					'method'  => 'POST',
+					'content' => $body,
+					'header'  => "Content-Type: application/x-www-form-urlencoded\r\n",
+					'timeout' => 60
+				     )
+			     );
+		$context = stream_context_create($opts);
+		return @file_get_contents($url, false, $context, -1);
+	}
 	public function binCall($func, $postData){
-		$this->errorMsg = false;
-		$url = WORDFENCE_API_URL . '/v' . WORDFENCE_VERSION . '/?' . $this->makeAPIQueryString() . '&action=' . $func;
-		$curl = curl_init($url);
-		curl_setopt ($curl, CURLOPT_TIMEOUT, 300);
-		//curl_setopt($curl, CURLOPT_VERBOSE, true);
-		curl_setopt ($curl, CURLOPT_USERAGENT, "Wordfence");
-		curl_setopt ($curl, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt ($curl, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($curl, CURLOPT_POST, true);
-		if($postData){                  
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
-		} else {                        
-			curl_setopt($curl, CURLOPT_POSTFIELDS, array());
-		}                               
-		$data = curl_exec($curl);       
-		$httpStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		$url = $this->getAPIURL() . '/v' . WORDFENCE_API_VERSION . '/?' . $this->makeAPIQueryString() . '&action=' . $func;
+		if(function_exists('curl_init')){
+			$curl = curl_init($url);
+			curl_setopt ($curl, CURLOPT_TIMEOUT, 300);
+			//curl_setopt($curl, CURLOPT_VERBOSE, true);
+			curl_setopt ($curl, CURLOPT_USERAGENT, "Wordfence");
+			curl_setopt ($curl, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt ($curl, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($curl, CURLOPT_POST, true);
+			if($postData){                  
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+			} else {                        
+				curl_setopt($curl, CURLOPT_POSTFIELDS, array());
+			}                               
+			$data = curl_exec($curl);       
+			$httpStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			if($httpStatus != 200){
+				$cError = curl_error($curl);
+				curl_close($curl);
+				throw new Exception(self::$maintMsg . " Got HTTP status [$httpStatus] and curl error: $cError");
+			}
+		} else {
+			$data = $this->fileGet($url, $postData);
+			if($data === false){
+				throw new Exception(self::$maintMsg . " Got HTTP error: " . error_get_last());
+			}
+			$httpStatus = '200';
+		}
 		if(preg_match('/\{.*errorMsg/', $data)){
 			$jdat = @json_decode($data, true);
 			if(is_array($jdat) && $jdat['errorMsg']){
-				$this->errorMsg = $jdat['errorMsg'];
-				return false;
+				throw new Exception($jdat['errorMsg']);
 			}
 		}
 		return array('code' => $httpStatus, 'data' => $data);
@@ -123,6 +146,14 @@ class wfAPI {
 			's' => $siteurl, 
 			'k' => $this->APIKey
 			));
+	}
+	private function getAPIURL(){
+		$ssl_supported = false;
+		if(defined('CURL_VERSION_SSL') && function_exists('curl_version')){
+			$version = curl_version();
+			$ssl_supported = ($version['features'] & CURL_VERSION_SSL);
+		}
+		return $ssl_supported ? WORDFENCE_API_URL_SEC : WORDFENCE_API_URL_NONSEC;
 	}
 }
 
--- wordfence.php
+++ wordfence.php
@@ -4,14 +4,19 @@ Plugin Name: Wordfence Security
 Plugin URI: http://wordfence.com/
 Description: Wordfence Security - Anti-virus and Firewall security plugin for WordPress 
 Author: Mark Maunder
-Version: 2.0.5
+Version: 3.0.8
 Author URI: http://wordfence.com/
 */
-require_once('lib/wordfenceConstants.php');
-require_once('lib/wordfenceClass.php');
-register_activation_hook(WP_PLUGIN_DIR . '/wordfence/wordfence.php', 'wordfence::installPlugin');
-register_deactivation_hook(WP_PLUGIN_DIR . '/wordfence/wordfence.php', 'wordfence::uninstallPlugin');
-wordfence::install_actions();
-
+define('WORDFENCE_VERSION', '3.0.8');
+if(! defined('WORDFENCE_VERSIONONLY_MODE')){
+	if((int) @ini_get('memory_limit') < 64){
+		@ini_set('memory_limit', '64M'); //Some hosts have ini set at as little as 32 megs. 64 is the min sane amount of memory.
+	}
+	require_once('lib/wordfenceConstants.php');
+	require_once('lib/wordfenceClass.php');
+	register_activation_hook(WP_PLUGIN_DIR . '/wordfence/wordfence.php', 'wordfence::installPlugin');
+	register_deactivation_hook(WP_PLUGIN_DIR . '/wordfence/wordfence.php', 'wordfence::uninstallPlugin');
+	wordfence::install_actions();
+}
 
 ?>
