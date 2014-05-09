e1738d7448add758d8f88a57a7ece8e31f0e253a
mmaunder <mmaunder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-09 16:17:42 +0000

    * Feature: Immediately block IP if hacker tries any of the following usernames. (Comma separated list that you can specify on the Wordfence options page) * Feature: Exclude exact URL's from caching. Specifically, this allows you to exclude the home page which was not possible before. * Feature: Exclude browsers or partial browser matches and specific cookies from caching. * Fix: Fixed issue where /.. dirs would be included in certain scandir operations. * Fix: logHuman function was not analyzing user-agent strings correctly which would allow some crawlers that execute JS to be logged as humans. * Fix: Removed ob_end_clean warnings about empty buffers when a human is being logged. * Fix: Removed warning in lib/wfCache.php caused by unset $_SERVER['QUERY_STRING'] when we check it. * Fix: Fixed "logged out as ''" blank username logout messages. * Fix: Improved security of config cache by adding a PHP header to file that we strip. Already secure because we have a .htaccess denying access, but more is better. * Fix: Falcon Engine option to clear Falcon cache when a post scheduled to be published in future is published. * Fix: Fixed Heartbleed scans hanging.
    git-svn-id: https://plugins.svn.wordpress.org/wordfence/tags/5.0.7@911163 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/email_newIssues.php
+++ lib/email_newIssues.php
@@ -1,3 +1,5 @@
+This email was sent from your website "<?php echo get_bloginfo('name', 'raw'); ?>" by the Wordfence plugin.
+
 Wordfence found the following new issues on "<?php echo get_bloginfo('name', 'raw'); ?>".
 
 Alert generated at <?php echo wfUtils::localHumanDate(); ?>
@@ -18,6 +20,21 @@ Warnings:
 
 <?php } } } ?>
 
+
+<?php if(! $isPaid){ ?>
+NOTE: You are using the free version of Wordfence. Upgrading to the paid version of Wordfence gives you 
+two factor authentication (sign-in via cellphone) and country blocking which are both effective methods to block attacks.
+A Premium Wordfence license also includes remote scanning with each scan of your site which can detect 
+several additional website infections. Premium members can also schedule when website scans occur and
+can scan more than once per day.
+
+As a Premium member you also get access to our priority support system located at http://support.wordfence.com/ and can file
+priority support tickets using our ticketing system. 
+
+Click here to sign-up for the Premium version of Wordfence now.
+https://www.wordfence.com/wordfence-signup/
+
+<?php } ?>
 
 
 
