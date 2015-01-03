e3487efb2985f7b242795b46be50cd501e59cd05
wfmatt <wfmatt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-03 03:15:34 +0000

    removed argument from cron hook
    git-svn-id: https://plugins.svn.wordpress.org/wordfence/branches/matt@1058831 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/wordfenceClass.php
+++ lib/wordfenceClass.php
@@ -1182,6 +1182,8 @@ class wordfence {
 	}
 	public static function wordfenceStartScheduledScan(){
 		
+		error_log('wordfenceStartScheduledScan: ' . var_export(func_get_args()), E_USER_NOTICE);
+		
 		//If scheduled scans are not enabled in the global config option, then don't run a scheduled scan.
 		if(wfConfig::get('scheduledScansEnabled') != '1'){
 			return;
@@ -1247,24 +1249,24 @@ class wordfence {
 		}
 	}
 	private static function scheduleSingleScan($futureTime){
-		wp_schedule_single_event($futureTime, 'wordfence_start_scheduled_scan', array($futureTime));
-		$schedArgs = wfConfig::get_ser('schedScanArgs', array());
-		if(! is_array($schedArgs)){ //paranoia
-			$schedArgs = array();
-		}
-		$schedArgs[] = $futureTime;
-		wfConfig::set_ser('schedScanArgs', $schedArgs);
+		wp_schedule_single_event($futureTime, 'wordfence_start_scheduled_scan');
+		// $schedArgs = wfConfig::get_ser('schedScanArgs', array());
+		// if(! is_array($schedArgs)){ //paranoia
+		// 	$schedArgs = array();
+		// }
+		// $schedArgs[] = $futureTime;
+		// wfConfig::set_ser('schedScanArgs', $schedArgs);
 	}
 	private static function unscheduleAllScans(){
 		wp_clear_scheduled_hook('wordfence_start_scheduled_scan'); //Unschedule legacy scans without args
 
-		$schedArgs = wfConfig::get_ser('schedScanArgs', array());
-		if(is_array($schedArgs)){
-			foreach($schedArgs as $futureTime){
-				wp_clear_scheduled_hook('wordfence_start_scheduled_scan', array($futureTime));
-			}
-		}
-		wfConfig::set_ser('schedScanArgs', array());
+		// $schedArgs = wfConfig::get_ser('schedScanArgs', array());
+		// if(is_array($schedArgs)){
+		// 	foreach($schedArgs as $futureTime){
+		// 		wp_clear_scheduled_hook('wordfence_start_scheduled_scan', array($futureTime));
+		// 	}
+		// }
+		// wfConfig::set_ser('schedScanArgs', array());
 	}
 	public static function ajax_saveCountryBlocking_callback(){
 		if(! wfConfig::get('isPaid')){
